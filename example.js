88fe31b930e8f2f158e34646c20e01eeae9b80bb
Sindre Sorhus <sindresorhus@gmail.com>
2017-07-23 13:52:49 +0200

    Add the readme example as a file

---
+++ example.js
@@ -0,0 +1,20 @@
+'use strict';
+const logUpdate = require('.');
+
+const frames = ['-', '\\', '|', '/'];
+let i = 0;
+
+setInterval(() => {
+	const frame = frames[i = ++i % frames.length];
+
+	logUpdate(
+`
+        ♥♥
+   ${frame} unicorns ${frame}
+        ♥♥
+`
+	);
+}, 80);
+
+// Run it with:
+// $ node example.js
