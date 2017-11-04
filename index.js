98be78d5201cb25202261b7264f77e5a0e46400a
Sindre Sorhus <sindresorhus@gmail.com>
2017-11-04 14:10:13 +0700

    Code improvements

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const ansiEscapes = require('ansi-escapes');
 const cliCursor = require('cli-cursor');
 const wrapAnsi = require('wrap-ansi');
 
+const getWidth = stream => {
+	const columns = stream.columns;
+
+	if (!columns) {
+		return 80;
+	}
+
+	// Windows appears to wrap a character early
+	// I hate Windows so much
+	if (process.platform === 'win32') {
+		return columns - 1;
+	}
+
+	return columns;
+};
+
 const main = (stream, options) => {
 	options = Object.assign({
 		showCursor: false
@@ -10,27 +26,17 @@ const main = (stream, options) => {
 
 	let prevLineCount = 0;
 
-	const getWidth = function (columns) {
-		// Default width to 80 if we can't get column count
-		if (!columns) {
-			return 80;
-		}
-
-		// Windows appears to wrap a character early
-		if (process.platform === 'win32') {
-			return columns - 1;
-		}
-
-		return columns;
-	};
-
 	const render = function () {
 		if (!options.showCursor) {
 			cliCursor.hide();
 		}
 
 		let out = [].join.call(arguments, ' ') + '\n';
-		out = wrapAnsi(out, getWidth(process.stdout.columns), {trim: false, hard: true, wordWrap: false});
+		out = wrapAnsi(out, getWidth(stream), {
+			trim: false,
+			hard: true,
+			wordWrap: false
+		});
 		stream.write(ansiEscapes.eraseLines(prevLineCount) + out);
 		prevLineCount = out.split('\n').length;
 	};
