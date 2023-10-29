dc1bb73d250effe2e181c1fb1425a8d866d7609e
Sindre Sorhus <sindresorhus@gmail.com>
2023-10-29 11:47:36 +0700

    Simplify

--- index.js
+++ index.js
@@ -7,29 +7,13 @@ import stripAnsi from 'strip-ansi';
 
 const defaultTerminalHeight = 24;
 
-const getWidth = stream => {
-	const {columns} = stream;
-
-	if (!columns) {
-		return 80;
-	}
-
-	return columns;
-};
+const getWidth = ({columns = 80}) => columns;
 
 const fitToTerminalHeight = (stream, text) => {
 	const terminalHeight = stream.rows ?? defaultTerminalHeight;
 	const lines = text.split('\n');
-
-	const toRemove = lines.length - terminalHeight;
-	if (toRemove <= 0) {
-		return text;
-	}
-
-	return sliceAnsi(
-		text,
-		stripAnsi(lines.slice(0, toRemove).join('\n')).length + 1,
-	);
+	const toRemove = Math.max(0, lines.length - terminalHeight);
+	return toRemove ? sliceAnsi(text, stripAnsi(lines.slice(0, toRemove).join('\n')).length + 1) : text;
 };
 
 export function createLogUpdate(stream, {showCursor = false} = {}) {
@@ -37,41 +21,39 @@ export function createLogUpdate(stream, {showCursor = false} = {}) {
 	let previousWidth = getWidth(stream);
 	let previousOutput = '';
 
+	const reset = () => {
+		previousOutput = '';
+		previousWidth = getWidth(stream);
+		previousLineCount = 0;
+	};
+
 	const render = (...arguments_) => {
 		if (!showCursor) {
 			cliCursor.hide();
 		}
 
-		let output = arguments_.join(' ') + '\n';
-		output = fitToTerminalHeight(stream, output);
+		let output = fitToTerminalHeight(stream, arguments_.join(' ') + '\n');
 		const width = getWidth(stream);
+
 		if (output === previousOutput && previousWidth === width) {
 			return;
 		}
 
 		previousOutput = output;
 		previousWidth = width;
-		output = wrapAnsi(output, width, {
-			trim: false,
-			hard: true,
-			wordWrap: false,
-		});
+		output = wrapAnsi(output, width, {trim: false, hard: true, wordWrap: false});
+
 		stream.write(ansiEscapes.eraseLines(previousLineCount) + output);
 		previousLineCount = output.split('\n').length;
 	};
 
 	render.clear = () => {
 		stream.write(ansiEscapes.eraseLines(previousLineCount));
-		previousOutput = '';
-		previousWidth = getWidth(stream);
-		previousLineCount = 0;
+		reset();
 	};
 
 	render.done = () => {
-		previousOutput = '';
-		previousWidth = getWidth(stream);
-		previousLineCount = 0;
-
+		reset();
 		if (!showCursor) {
 			cliCursor.show();
 		}
